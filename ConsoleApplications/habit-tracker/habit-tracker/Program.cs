1fea16d93dd7b8e4276b011d57467bed4c361dd6
Marius Visan <vsnmrs@hotmail.com>
2022-08-04 22:32:18 +0300

    Added the SQLite library
    Added code to:
    - create a database and connect to it
    - create the habits table
    - get the SQLite version

--- ConsoleApplications/habit-tracker/habit-tracker/Program.cs
+++ ConsoleApplications/habit-tracker/habit-tracker/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace habit_tracker
+﻿using System.Data.SQLite;
+
+namespace habit_tracker
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SQLiteConnection connection = CreateConnection();
+            GetSQLiteVersion(connection);
+            CreateHabbitsTable(connection);
+        }
+
+        private static SQLiteConnection CreateConnection()
+        {
+            var connection = new SQLiteConnection("Data Source=habits.db");
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return connection;
+        }
+
+        private static void GetSQLiteVersion(SQLiteConnection connection)
+        {
+            var command = new SQLiteCommand("SELECT SQLITE_VERSION()", connection);
+            string? version = command.ExecuteScalar().ToString();
+
+            Console.WriteLine($"SQLite version {version}");
+        }
+
+        private static void CreateHabbitsTable(SQLiteConnection connection)
+        {
+            var command = new SQLiteCommand(connection);
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS habits(id INTEGER PRIMARY KEY, name TEXT, value INT)";
+            command.ExecuteNonQuery();
         }
     }
 }
