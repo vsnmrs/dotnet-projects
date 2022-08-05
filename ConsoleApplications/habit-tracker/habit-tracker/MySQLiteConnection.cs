3c919e21744269102064f3c085069c8d2c098dce
Marius Visan <vsnmrs@hotmail.com>
2022-08-05 11:05:36 +0300

    Moved the SQLite code to its own class

---
+++ ConsoleApplications/habit-tracker/habit-tracker/MySQLiteConnection.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SQLite;
+
+namespace habit_tracker
+{
+    public class MySQLiteConnection
+    {
+        private SQLiteConnection _connection;
+
+        public MySQLiteConnection(string dbName)
+        {
+            _connection = new SQLiteConnection($"Data Source={dbName}");
+
+            try
+            {
+                _connection.Open();
+                Console.WriteLine($"Connection to the {dbName} database established!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void GetSQLiteVersion()
+        {
+            var command = new SQLiteCommand("SELECT SQLITE_VERSION()", _connection);
+            string? version = command.ExecuteScalar().ToString();
+
+            Console.WriteLine($"SQLite version {version}");
+        }
+
+        public void CreateHabbitsTable()
+        {
+            var command = new SQLiteCommand(_connection);
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS habits(id INTEGER PRIMARY KEY, name TEXT, value INT)";
+            command.ExecuteNonQuery();
+        }
+    }
+}
--- ConsoleApplications/habit-tracker/habit-tracker/Program.cs
+++ ConsoleApplications/habit-tracker/habit-tracker/Program.cs
@@ -6,40 +6,9 @@ namespace habit_tracker
     {
         static void Main(string[] args)
         {
-            SQLiteConnection connection = CreateConnection();
-            GetSQLiteVersion(connection);
-            CreateHabbitsTable(connection);
-        }
-
-        private static SQLiteConnection CreateConnection()
-        {
-            var connection = new SQLiteConnection("Data Source=habits.db");
-
-            try
-            {
-                connection.Open();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            return connection;
-        }
-
-        private static void GetSQLiteVersion(SQLiteConnection connection)
-        {
-            var command = new SQLiteCommand("SELECT SQLITE_VERSION()", connection);
-            string? version = command.ExecuteScalar().ToString();
-
-            Console.WriteLine($"SQLite version {version}");
-        }
-
-        private static void CreateHabbitsTable(SQLiteConnection connection)
-        {
-            var command = new SQLiteCommand(connection);
-            command.CommandText = @"CREATE TABLE IF NOT EXISTS habits(id INTEGER PRIMARY KEY, name TEXT, value INT)";
-            command.ExecuteNonQuery();
+            MySQLiteConnection _sqliteConnection = new MySQLiteConnection("habits.db");
+            _sqliteConnection.GetSQLiteVersion();
+            _sqliteConnection.CreateHabbitsTable();
         }
     }
 }
