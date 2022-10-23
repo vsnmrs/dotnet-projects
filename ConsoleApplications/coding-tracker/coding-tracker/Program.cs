b60c1d1985941744a390fa3b828d7b207e5a03d8
Marius Visan <vsnmrs@hotmail.com>
2022-10-23 17:43:48 +0300

    Created the coding-tracker project

---
+++ ConsoleApplications/coding-tracker/coding-tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace coding_tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
