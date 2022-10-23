e493254d46e7962384404a535866b1877da626db
Marius Visan <vsnmrs@hotmail.com>
2022-10-23 18:32:33 +0300

    Added a configuration file for the program
    Holds info as database path and connection string

--- ConsoleApplications/coding-tracker/coding-tracker/Program.cs
+++ ConsoleApplications/coding-tracker/coding-tracker/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace coding_tracker
+﻿using System.Configuration;
+using System.Collections.Specialized;
+
+namespace coding_tracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? databasePath = ConfigurationManager.AppSettings.Get("databasePath");
+            string? databaseConnectionString = ConfigurationManager.AppSettings.Get("connectionString");
+
+            Console.WriteLine(databasePath + " " + databaseConnectionString);
         }
     }
 }
