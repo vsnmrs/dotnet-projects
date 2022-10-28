bbbfc5d8ac9fb023437aabd69e5fab9155629837
Marius Visan <vsnmrs@hotmail.com>
2022-10-28 16:00:09 +0300

    Added code to convert a date from string to DateTime format

--- ConsoleApplications/coding-tracker/coding-tracker/CodingSessionRecord.cs
+++ ConsoleApplications/coding-tracker/coding-tracker/CodingSessionRecord.cs
@@ -3,16 +3,16 @@
     public class CodingSessionRecord
     {
         private int _id;
-        private string _sessionStart, _sessionEnd;
+        private DateTime _sessionStart, _sessionEnd;
         private int _sessionDuration; //in minutes, is auto-calculated, not entered manually
 
         private int ID { get { return _id; } }
         private int SessionDuration { get { return _sessionDuration; } }
 
-        private string SessionStart { get { return _sessionStart; } }
-        private string SessionEnd { get { return _sessionEnd; } }
+        private DateTime SessionStart { get { return _sessionStart; } }
+        private DateTime SessionEnd { get { return _sessionEnd; } }
 
-        public CodingSessionRecord(string sessionStart, string sessionEnd)
+        public CodingSessionRecord(DateTime sessionStart, DateTime sessionEnd)
         {
             _sessionStart = sessionStart;
             _sessionEnd = sessionEnd;
--- ConsoleApplications/coding-tracker/coding-tracker/UserInput.cs
+++ ConsoleApplications/coding-tracker/coding-tracker/UserInput.cs
@@ -1,6 +1,8 @@
-﻿namespace coding_tracker
+﻿using System.Globalization;
+
+namespace coding_tracker
 {
-    public static class UserInput
+    public class UserInput
     {
         public static void GetUserInput()
         {
@@ -20,11 +22,35 @@
                         Console.WriteLine("Exiting program!");
                         closeApplication = true;
                         break;
+                    case "1":
+                        Console.WriteLine("Enter start time using the following format: dd/MM/year");
+                        string date = Console.ReadLine();
+                        DateTime d;
+                        while (!ConvertStringToDateTime(date, out d))
+                        {
+                            Console.WriteLine("Incorrect date or time, try again! dd/MM/year");
+                            date = Console.ReadLine();
+                        }
+
+                        Console.WriteLine(d.ToString());
+                        break;
                     default:
                         Console.WriteLine("Command not recognized, try again!");
                         break;
                 }
             }
+        }
+
+        private static bool ConvertStringToDateTime(string dateString, out DateTime dTime)
+        {;
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            bool result = DateTime.TryParseExact(dateString, "dd/MM/yyyy", culture, DateTimeStyles.None, out dTime);
+
+            if (result)
+                return true;
+            else
+                return false;
         }
     }
 }
